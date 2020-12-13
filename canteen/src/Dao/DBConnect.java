35fa4da6bb383f1a3549d8e2950de8890caabf54
xinrf1 <xinrf1@lenovo.com>
2020-12-13 13:01:14 +0800

    admin view

--- canteen/src/Dao/DBConnect.java
+++ canteen/src/Dao/DBConnect.java
@@ -11,12 +11,12 @@ public class DBConnect  {
 		return connection;
 	}
 	
-	private static String url ="jdbc:mysql://www.papademas.net:3307/510fp?autoReconnect=true&useSSL=false";
-	private static String username = "fp510";
-	private static String password = "510";
-//	private static String url = "jdbc:mysql://localhost:3306/510fp";
-//	private static String username = "root";
-//	private static String password = "526563Kk~!";
+//	private static String url ="jdbc:mysql://www.papademas.net:3307/510fp?autoReconnect=true&useSSL=false";
+//	private static String username = "fp510";
+//	private static String password = "510";
+	private static String url = "jdbc:mysql://localhost:3306/510fp";
+	private static String username = "root";
+	private static String password = "526563Kk~!";
 
 	public DBConnect() {
 		try {
